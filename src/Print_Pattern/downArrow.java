ccd9e07b573a6fbc46c8f25328eca23746c9f0f2
dondasasmita <dondasasmita@gmail.com>
2018-03-03 11:01:35 +0800

    down arrow pattern

---
+++ src/Print_Pattern/downArrow.java
@@ -0,0 +1,13 @@
+package Print_Pattern;
+
+public class downArrow {
+
+    public static void main(String[] args) {
+
+        String anyString = "Indonesia-Bali-Ubud";
+
+        for (int i = 0 ; i <= anyString.length() ; i++) {
+            System.out.println(anyString.substring(i));
+        }
+    }
+}
